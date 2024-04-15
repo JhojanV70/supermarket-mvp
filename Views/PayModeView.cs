cfb706c53a3e0d69f3e4c47df584f893631d4291
JhojanV70 <jhojanv@gmail.com>
2024-04-14 22:39:43 -0500

    crear fomulario PayModeView

---
+++ Views/PayModeView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Supermarket_mvp.Views
+{
+    public partial class PayModeView : Form
+    {
+        public PayModeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
