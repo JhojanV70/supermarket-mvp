a33125bc8a748c872cee0f7f6cbec97d5b94c53a
JhojanV70 <jhojanv@gmail.com>
2024-04-14 21:36:31 -0500

    Agregar pripiedades a la clase PayModeModel

--- Models/PayModeModel.cs
+++ Models/PayModeModel.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace Supermarket_mvp.Models
 {
     internal class PayModeModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Observation { get; set; }
+
     }
 }
