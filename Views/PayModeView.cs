ba14ec4a8e7658ffc4fecf2d11f8893de5a71e94
JhojanV70 <jhojanv@gmail.com>
2024-04-15 08:36:36 -0500

    Se agrega la Interface 'IPayModeView' y sus metodos-1

--- Views/PayModeView.cs
+++ Views/PayModeView.cs
@@ -8,11 +8,36 @@ using System.Windows.Forms;
 
 namespace Supermarket_mvp.Views
 {
-    public partial class PayModeView : Form
+    public partial class PayModeView : Form, IPayModeView
     {
         public PayModeView()
         {
             InitializeComponent();
+
         }
+
+        
+
+        public string PayModeId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string PayModeName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string PayModeObservation { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SearchValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsEdit { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsSuccessful { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Message { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event EventHandler SearchEcent;
+        public event EventHandler AddNewEvent;
+        public event EventHandler EditEvent;
+        public event EventHandler DeleteEvent;
+        public event EventHandler SaveEvent;
+        public event EventHandler CancelEvent;
+
+        public void SetPayModeListBildingSource(BindingSource payModeList)
+        {
+            DgPayMode.DataSource = payModeList;
+        }
+
+
     }
 }
