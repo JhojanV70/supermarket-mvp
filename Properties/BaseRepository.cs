a5dc437c1bf2c74e8a208746692f125acf827628
JhojanV70 <jhojanv@gmail.com>
2024-04-15 22:06:30 -0500

    Implementacion de la Interface IPayModeRepository

--- Properties/BaseRepository.cs
+++ Properties/BaseRepository.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.sqlClient;
+using Microsoft.Data;
+using Supermarket_mvp.Models;
+using System.Data;
 
 namespace Supermarket_mvp.Properties
 {
-    internal class BaseRepository
+    internal class BaseRepository : IPayModeRepository
     {
         protected string connectionString;
+
+        void IPayModeRepository.Add(PayModeModel payModeModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IPayModeRepository.Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IPayModeRepository.Edit(PayModeModel payModeModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<PayModeModel> IPayModeRepository.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<PayModeModel> IPayModeRepository.GetByValue(string value)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
