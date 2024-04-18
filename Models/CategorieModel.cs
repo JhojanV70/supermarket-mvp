f743635318df0992c4f04c2ca75a3819467fbf22
JhojanV70 <jhojanv@gmail.com>
2024-04-18 08:29:42 -0500

    Crear clase Detail y componentes

--- Models/CategorieModel.cs
+++ Models/CategorieModel.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace Supermarket_mvp.Models
 {
     internal class CategorieModel
     {
+        [DisplayName("Categorie Id")]
+        public int Id { get; set; }
+
+        [DisplayName ("Categorie Name")]
+        [Required(ErrorMessage = "Categorie name is required")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Categorie name must be between 3 and 200 characters")]
+        public string Name { get; set; }
+        [DisplayName ("Description")]
+        [Required(ErrorMessage = "Categorie description is required")]
+        [StringLength(200, MinimumLength = 3 , ErrorMessage = "Categorie description must be beween3 and 200 characters")]
+        public string Description { get; set; }
     }
 }
---
+++ Models/DetailModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal class DetailModel
+    {
+        [DisplayName("Datail Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Detail Qantity")]
+        [Required(ErrorMessage = "Detail Quantity is Required")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Detail quantity must be between 1 and  100 characters")]
+        public string Quantity { set; get; }
+
+        [DisplayName ("Detail Price")]
+        [Required(ErrorMessage = "Detail Price is Required")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Detail Price must be between 1 and 100 characters")]
+        public string Price { set; get; }
+    }
+}
