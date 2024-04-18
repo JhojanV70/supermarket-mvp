8c7afee1cd435d3bca1df9f746ed91651f1c3e6d
JhojanV70 <jhojanv@gmail.com>
2024-04-18 08:43:51 -0500

    Crear Interface IDetailRepository y sus componentes

---
+++ Models/IDetailRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal interface IDetailRepository
+    {
+        void Add(DetailModel detail);
+        void Edit(DetailModel detail);
+        void Delete(int id);
+        IEnumerable<DetailModel> GetAll();
+        IEnumerable<DetailModel> GetByValue(string value);
+    }
+}
