14a54006c957609dfb849fd1979ddf9f0a186adc
JhojanV70 <jhojanv@gmail.com>
2024-04-18 13:50:44 -0500

    crear metodo constructor 'DetailPresenter' y sus componentes logicos

--- Presenters/DetailPresenter.cs
+++ Presenters/DetailPresenter.cs
@@ -9,5 +9,26 @@ namespace Supermarket_mvp.Presenters
 {
     internal class DetailPresenter
     {
+        private IDetailView view;
+        private IDetailRepository repository;
+        private BindingSource detailBindingSource;
+        private IEnumerable<DetailModel> detailList;
+
+        public DetailPresenter(IDetailView view, IDetailRepository repository)
+        {
+            this.detailBindingSource = new BindingSource();
+
+            this.view = view;
+            this.repository = repository;
+
+            this.view.SearchEvent += SearchDetail;
+            this.view.AddNewEvent += AddNewDetail;
+            this.view.EditEvent += LoadSelectDetailToEdit;
+            this.view.DeleteEvent += DeleteSelectedDetail;
+            this.view.SaveEvent += SaveDetail;
+            this.view.CancelEvent += CancelAction;
+
+            this.view.SetDetailListBildingSource(detailBindingSource);
+        }
     }
 }
