7440767eb2141ad1c6492ad9821890b9dbf07dbd
JhojanV70 <jhojanv@gmail.com>
2024-04-18 14:50:29 -0500

    crear clase CategorieRepository e Interface

---
+++ _Repositories/CategorieRepository.cs
@@ -0,0 +1,37 @@
+﻿using Supermarket_mvp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp._Repositories
+{
+    internal class CategorieRepository : BaseRepository, ICategorieRepository
+    {
+        public void Add(CategorieModel categorieModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(CategorieModel categorieModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CategorieModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CategorieModel> GetByValue(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
