7dd6518748bec8dc40d04a254d93d91a3f898887
JhojanV70 <jhojanv@gmail.com>
2024-04-18 13:41:36 -0500

    crear metodo constructor 'CategoriePresenter' y sus componentes logicos

--- Presenters/CategoriePresenter.cs
+++ Presenters/CategoriePresenter.cs
@@ -10,5 +10,27 @@ namespace Supermarket_mvp.Presenters
 {
     internal class CategoriePresenter
     {
+        private ICategorieView view;
+        private ICategorieRepository repository;
+        private BindingSource categorieBindingSource;
+        private IEnumerable<CategorieModel> categorieList;
+
+        public CategoriePresenter(ICategorieView view, ICategorieRepository repository)
+        {
+            this.categorieBindingSource = new BindingSource();
+
+            this.view = view;
+            this.repository = repository;
+
+            this.view.SearchEvent += SearchCategorie;
+            this.view.AddNewEvent += AddNewCategorie;
+            this.view.EditEvent += LoadSelectCategorieToEdit;
+            this.view.DeleteEvent += DeleteSelectedCategorie;
+            this.view.SaveEvent += SaveCategorie;
+            this.view.CancelEvent += CancelAction;
+
+            this.view.SetCategorieListBildingSource(categorieBindingSource);
+        }
     }
+
 }
