c163faf4d79ea696b249ebf2ba5162bf470e48b1
JhojanV70 <jhojanv@gmail.com>
2024-04-18 11:16:37 -0500

    Implementacion de Interface ICategorieView y sus propiedades

--- Views/CategorieView.cs
+++ Views/CategorieView.cs
@@ -10,11 +10,64 @@ using System.Windows.Forms;
 
 namespace Supermarket_mvp.Views
 {
-    public partial class CategorieView : Form
+    public partial class CategorieView : Form, ICategorieView
     {
+        private bool isEdit;
+        private bool isSuccessful;
+        private string message;
         public CategorieView()
         {
             InitializeComponent();
         }
+        
+        public string CategorieId
+        {
+            get { return TxtCategorieId.Text; }
+            set { TxtCategorieId.Text = value; }
+        }
+        public string CategorieName
+        {
+            get { return TxtCategorieName.Text; }
+            set { TxtCategorieName.Text = value; }
+        }
+        public string CategorieDescription
+        {
+            get { return TxtCategorieDescription.Text; }
+            set { TxtCategorieDescription.Text = value; }
+        }
+        public string SearchValue
+        {
+            get { return TxtSearch.Text; }
+            set { TxtSearch.Text = value; }
+        }
+        public bool IsEdit
+        {
+            get { return isEdit; }
+            set { isEdit = value; }
+        }
+        public bool IsSuccessful
+        {
+            get { return isSuccessful; }
+            set { isSuccessful = value; }
+        }
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+
+        public event EventHandler SearchEvent;
+        public event EventHandler AddNewEvent;
+        public event EventHandler EditEvent;
+        public event EventHandler DeleteEvent;
+        public event EventHandler SaveEvent;
+        public event EventHandler CancelEvent;
+
+        public void SetCategorieListBildingSource(BindingSource categorieList)
+        {
+            DgCategorie.DataSource = categorieList;
+        }
+
+
     }
 }
