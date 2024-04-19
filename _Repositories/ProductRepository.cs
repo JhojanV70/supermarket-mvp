481150f53905a5850a4be891c21c0545f907ac2d
JhojanV70 <jhojanv@gmail.com>
2024-04-19 17:08:22 -0500

    Crear metodo GetAll y sus componentes

--- _Repositories/ProductRepository.cs
+++ _Repositories/ProductRepository.cs
@@ -11,6 +11,11 @@ namespace Supermarket_mvp._Repositories
 {
     internal class ProductRepository : BaseRepository, IProductRepository
     {
+        public ProductRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
         public void Add(ProductModel productModel)
         {
             throw new NotImplementedException();
@@ -28,7 +33,27 @@ namespace Supermarket_mvp._Repositories
 
         public IEnumerable<ProductModel> GetAll()
         {
-            throw new NotImplementedException();
+            var productList = new List<ProductModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Product ORDER BY Detail_Id DESC";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var productModel = new ProductModel();
+                        productModel.Id = (int)reader["Detail_Id"];
+                        productModel.Name = reader["Detail_Name"].ToString();
+                        productModel.Price = (int)reader["Detail_Price"];
+                        productModel.Stock = (int)reader["Detail_Stock"];
+                        productList.Add(productModel);
+                    }
+                }
+            }
+            return productList;
         }
 
         public IEnumerable<ProductModel> GetByValue(string value)
