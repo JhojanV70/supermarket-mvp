f8ba18cab8828c2e61176bdf09a6142316101874
JhojanV70 <jhojanv@gmail.com>
2024-04-19 16:06:52 -0500

    crear clase ProductPresenter y sus metodos

---
+++ Presenters/ProductPresenter.cs
@@ -0,0 +1,26 @@
+﻿using Supermarket_mvp.Models;
+using Supermarket_mvp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Presenters
+{
+    internal class ProductPresenter
+    {
+        private IProductView view;
+        private IProductRepository repository;
+        private BindingSource productBindingSource;
+        private IEnumerable<ProductModel> productlList;
+
+        public ProductPresenter(IProductView view, IProductRepository repository)
+        {
+            this.productBindingSource = new BindingSource();
+
+            this.view = view;
+            this.repository = repository;
+        }
+    }
+}
