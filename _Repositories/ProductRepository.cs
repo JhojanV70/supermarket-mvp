16edea32bf2b3974891b1420b6aa61a538bde2ef
JhojanV70 <jhojanv@gmail.com>
2024-04-19 16:33:28 -0500

    Crear clase ProductRepository

---
+++ _Repositories/ProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Supermarket_mvp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Supermarket_mvp._Repositories
+{
+    internal class ProductRepository
+    {
+    }
+}
